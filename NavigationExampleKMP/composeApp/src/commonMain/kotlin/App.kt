f44edd57e4747558d635841f5429b88038940e7e
LiizethRR <rmm.che@gmail.com>
2025-03-31 14:05:37 -0600

    Primer commit

--- NavigationExampleKMP/composeApp/src/commonMain/kotlin/App.kt
+++ NavigationExampleKMP/composeApp/src/commonMain/kotlin/App.kt
@@ -1,21 +1,10 @@
 import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -26,21 +15,14 @@ import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.navigator.LocalNavigator
 import cafe.adriel.voyager.navigator.Navigator
 import cafe.adriel.voyager.navigator.currentOrThrow
-import cafe.adriel.voyager.transitions.FadeTransition
-import cafe.adriel.voyager.transitions.ScaleTransition
 import cafe.adriel.voyager.transitions.SlideTransition
-import org.jetbrains.compose.resources.ExperimentalResourceApi
-import org.jetbrains.compose.resources.painterResource
 import settings.ProfileScreen
 
 @Composable
 fun App() {
     MaterialTheme {
-        Navigator(screen = MainScreen())
-        {navigator ->
+        Navigator(screen = MainScreen()) { navigator ->
             SlideTransition(navigator)
-//            FadeTransition(navigator)
-//            ScaleTransition(navigator)
         }
     }
 }
@@ -50,23 +32,16 @@ class MainScreen : Screen {
     @Composable
     override fun Content() {
         val navigator = LocalNavigator.currentOrThrow
-        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+        Column(
+            Modifier.fillMaxSize().background(Color(0xFFF5F5DC)), // Beige
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
             Button(onClick = {
                 navigator.push(SecondScreen())
             }) {
                 Text("Navegación básica")
-            }
-            Spacer(Modifier.height(18.dp))
-            Button(onClick = {
-                navigator.push(BottomBarScreen())
-            }) {
-                Text("BottomBar")
-            }
-            Spacer(Modifier.height(18.dp))
-            Button(onClick = {
-                navigator.push(ProfileScreen())
-            }) {
-                Text("Navegación con persistencia")
+
             }
         }
     }
@@ -78,13 +53,13 @@ class SecondScreen : Screen {
     override fun Content() {
         val navigator = LocalNavigator.currentOrThrow
         Column(
-            modifier = Modifier.fillMaxSize().background(Color.Blue),
-            horizontalAlignment = Alignment.CenterHorizontally
+            modifier = Modifier.fillMaxSize().background(Color(0xFFF5F5DC)), // Beige
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
         ) {
-            Text("Segunda Pantalla", fontSize = 26.sp, color = Color.White)
+            Text("Segunda Pantalla", fontSize = 26.sp, color = Color.Black)
             Spacer(modifier = Modifier.height(16.dp))
             Button(onClick = { navigator.pop() }) { Text("Volver") }
         }
     }
-
 }
